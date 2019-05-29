dd85de4972893a2a4a851129a3f320ddee40643a
Njoki Kiarie <gnjokikiarie@gmail.com>
2019-05-29 17:22:58 +0300

    indentation

--- app.py
+++ app.py
@@ -1,33 +1,36 @@
+import pymysql  # import pymsql to install on the current project
+from werkzeug.utils import secure_filename
 import os
-from os.path import join,dirname,realpath
+from os.path import join, dirname, realpath
 
-from flask import Flask ,render_template ,flash,redirect,request , session  #flask-framework ,#Flask-python class
+# flask-framework ,#Flask-python class
+from flask import Flask, render_template, flash, redirect, request, session
 
-app = Flask(__name__) # we create a flask application parsing its name
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/?>' #used in encrypting the session
+app = Flask(__name__)  # we create a flask application parsing its name
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/?>'  # used in encrypting the session
 
 #saving files in the database
-from werkzeug.utils import secure_filename
-UPLOAD_FOLDER =join(dirname(realpath(__file__)), 'static/uploads/')
+UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'static/uploads/')
 
 #specify allowed extensions
-ALLOWED_EXTENSIONS =set(['epub','pdf','txt'])
+ALLOWED_EXTENSIONS = set(['epub', 'pdf', 'txt'])
 
 #configure upload folder in the app
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH']= 3*1024*1024
+app.config['MAX_CONTENT_LENGTH'] = 3*1024*1024
 
 #CHECK IF FILENAME UPLOADED MEETS ALLOWED EXTENSIONS
 
+
 def allowed_file(filename):
     return '.'in filename and \
-    filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-import pymysql    #import pymsql to install on the current project
-con= pymysql.connect("localhost", "root", "", "library")#establish db connection
-cursor=con.cursor()#execute sql -create a cursor object to execute sql
+# establish db connection
+con = pymysql.connect("localhost", "root", "", "library")
+cursor = con.cursor()  # execute sql -create a cursor object to execute sql
 
 
 @app.route('/')
@@ -35,66 +38,66 @@ def home():
     return render_template('home.html')
 
 
-
 @app.route('/books')
 def books():
-    sql= "SELECT * FROM `books` ORDER BY `title` DESC"
+    sql = "SELECT * FROM `books` ORDER BY `title` DESC"
 
-    cursor.execute(sql) #EXECUTE SQL
+    cursor.execute(sql)  # EXECUTE SQL
 
-        #count the returned rows
+    #count the returned rows
     if cursor.rowcount < 1:
-        return render_template('books.html', msg= " no books available")
+        return render_template('books.html', msg=" no books available")
     else:
-        rows =cursor.fetchall()
-            #send the rows to the presentation layer,your html
-        return render_template('books.html', rows= rows)
-
+        rows = cursor.fetchall()
+    print(rows)
+    #send the rows to the presentation layer,your html
+    return render_template('books.html', rows=rows)
 
 
 #route for book add
-@app.route('/addbooks', methods=['POST','GET'])
-def addbooks():#logic goes her
+@app.route('/addbooks', methods=['POST', 'GET'])
+def addbooks():  # logic goes her
     #handle form data
 
-     if request.method=='POST': #check if user posted something
-         title=request.form['title']
-         author=request.form['author']
+     if request.method == 'POST':  # check if user posted something
+         title = request.form['title']
+         author = request.form['author']
          genre = request.form['genre']
-         description=request.form['description']
-         file= request.files['file'] #receives file
+         description = request.form['description']
+         file = request.files['file']  # receives file
          # #check if file is present and allowed
          if file and allowed_file(file.filename):
-             filename=secure_filename(file.filename)
+             filename = secure_filename(file.filename)
              #saves the file and its filename
-             file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
              #once the file is saved, save the link to database0
 
-
          #validation
-         if title=="":
+         if title == "":
              return render_template('books.html', msg1="title field is empty")
              #flash("email field is empty")
 
-         if author=="":
+         if author == "":
              #flash("name field is empty")
              return render_template('books.html', msg2="author field is empty")
-         if file=="":
+         if file == "":
              #flash("name field is empty")
 
              return render_template('books.html', msg3="book field is empty")
 
          else:
-             con = pymysql.connect("localhost", "root", "", "library")  # establish db connection
+             # establish db connection
+             con = pymysql.connect("localhost", "root", "", "library")
              cursor = con.cursor()  # execute sql -create a cursor object to execute sql
              #save the three ``fields to the database
-             sql= "INSERT INTO `books`(`title`,`author`,`description`, `file`, `genre`) VALUES (%s,%s,%s, %s, %s)"  # %s protects data .
+             # %s protects data .
+             sql = "INSERT INTO `books`(`title`,`author`,`description`, `file`, `genre`) VALUES (%s,%s,%s, %s, %s)"
              try:
 
-
-                cursor.execute(sql,(title,author,description,filename,genre))
-                con.commit() #commits the changes to the db
-                return render_template('books.html' ,msg="add successful" )
+                cursor.execute(
+                    sql, (title, author, description, filename, genre))
+                con.commit()  # commits the changes to the db
+                return render_template('books.html', msg="add successful")
 
              except:
                con.rollback()
@@ -103,59 +106,59 @@ def addbooks():#logic goes her
 
 #book display function
 
-@app.route('/search' , methods=['POST','GET'])
-def search():
-    if request.method=='POST':
 
-        general=request.form['general']
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    if request.method == 'POST':
+
+        general = request.form['general']
 
         con = pymysql.connect("localhost", "root", "", "library")
         cursor = con.cursor()
 
-        sql= "SELECT * FROM books WHERE title like '%%s%' or author= %s ORDER BY  `id` DESC "
-        cursor.execute(sql,(general,general))
+        sql = "SELECT * FROM books WHERE title like '%%s%' or author= %s ORDER BY  `id` DESC "
+        cursor.execute(sql, (general, general))
 
         #check if cursor has zero rows
-        if cursor.rowcount==0:
-            return render_template('books.html',msg="no book with that title found")
+        if cursor.rowcount == 0:
+            return render_template('books.html', msg="no book with that title found")
         else:
-            rows=cursor.fetchall()
-            return render_template('books.html',rows=rows)
-    else:return render_template('books.html')
-
-
+            rows = cursor.fetchall()
+            return render_template('books.html', rows=rows)
+    else:
+        return render_template('books.html')
 
 
 #registration
 
 
-@app.route('/registration' , methods=['POST','GET'])
+@app.route('/registration', methods=['POST', 'GET'])
 def registration():
-    if request.method=='POST':
-        username=request.form['username']
-        email=request.form['email']
-        password=request.form['password']
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
 
         con = pymysql.connect("localhost", "root", "", "library")
         cursor = con.cursor()
 
-
-        sql="INSERT INTO `registration`(`username`,`email`,`password`) VALUES (%s,%s,%s)"
+        sql = "INSERT INTO `registration`(`username`,`email`,`password`) VALUES (%s,%s,%s)"
         try:
-            cursor.execute(sql,(username,email,password))
+            cursor.execute(sql, (username, email, password))
             con.commit()
-            return render_template('home.html', message="welcome" )
+            return render_template('home.html', message="welcome")
         except:
             con.rollback()
     else:
-        return render_template('registration.html' )
+        return render_template('registration.html')
 #login route
 
-@app.route('/login' , methods=['POST', 'GET'])
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
 
-        username= request.form['username']
+        username = request.form['username']
         password = request.form['password']
 
         con = pymysql.connect("localhost", "root", "", "library")
@@ -163,22 +166,18 @@ def login():
         sql = "SELECT * FROM `registration` where `username`=%s and `password`=%s"
 
         #execute sql using the cursor object
-        cursor.execute(sql,(username,password))
+        cursor.execute(sql, (username, password))
         #check if a match was found or not
-        if cursor.rowcount==0:
-            return render_template("login.html" , msg1="No match .Wrong input")
-        elif cursor.rowcount==1:
+        if cursor.rowcount == 0:
+            return render_template("login.html", msg1="No match .Wrong input")
+        elif cursor.rowcount == 1:
             # create a session for the user
             #we store username in session variable, you dont store password in session
 
-
-            session['userkey'] =username
-
-
-
+            session['userkey'] = username
 
             return redirect('/books')
-        elif cursor.rowcount >1:
+        elif cursor.rowcount > 1:
             return render_template('login.html', msg1="try again later")
         else:
             return render_template('login.html', msg1="contact admin")
@@ -189,9 +188,10 @@ def login():
 
 #logout
 
+
 @app.route('/logout')
 def logout():
-    session.pop('userkey',None)
+    session.pop('userkey', None)
     return redirect('login')
 
 
